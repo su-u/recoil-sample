faf87175b3caa2d3967fc508b1366b9cf0712b62
su- <aaaiiiuuu35@gmail.com>
2020-05-16 22:38:58 +0900

    Refacotr: ファイル分割

--- src/App.tsx
+++ src/App.tsx
@@ -7,51 +7,12 @@ import {
   useSetRecoilState
 } from "recoil";
 import "./styles.css";
-
-const todoListFilterState = atom({
-  key: "todoListFilterState",
-  default: "Show All"
-});
-
-const todoListState = atom({
-  key: "todoListState",
-  default: []
-});
-
-const filteredTodoListState = selector({
-  key: "filteredTodoListState",
-  get: ({ get }) => {
-    const filter = get(todoListFilterState);
-    const list = get(todoListState);
-
-    switch (filter) {
-      case "Show Completed":
-        return list.filter(item => item.isComplete);
-      case "Show Uncompleted":
-        return list.filter(item => !item.isComplete);
-      default:
-        return list;
-    }
-  }
-});
-
-const todoListStatsState = selector({
-  key: "todoListStatsState",
-  get: ({ get }) => {
-    const todoList = get(filteredTodoListState);
-    const totalNum = todoList.length;
-    const totalCompletedNum = todoList.filter(item => item.isComplete).length;
-    const totalUncompletedNum = totalNum - totalCompletedNum;
-    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
-
-    return {
-      totalNum,
-      totalCompletedNum,
-      totalUncompletedNum,
-      percentCompleted
-    };
-  }
-});
+import {
+  todoListFilterState,
+  todoListState,
+  filteredTodoListState,
+  todoListStatsState
+} from "./state/todo";
 
 const TodoList = () => {
   // changed from todoListState to filteredTodoListState
---
+++ src/state/todo.ts
@@ -0,0 +1,52 @@
+import {
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState
+} from "recoil";
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All"
+});
+
+export const todoListState = atom({
+  key: "todoListState",
+  default: []
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter(item => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter(item => !item.isComplete);
+      default:
+        return list;
+    }
+  }
+});
+
+export const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(filteredTodoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter(item => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted
+    };
+  }
+});
